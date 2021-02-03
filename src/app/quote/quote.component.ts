a6dc7c81f35ba789ba9c5c8b3b1f3959c1a1b623
254Davidhashisoma <davidhashisoam95@gmail.com>
2021-02-03 17:10:50 +0300

    style forms

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,10 +9,10 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
-    new Quote(1, 'Gideon Makori', "“Education is the most powerful weapon which you can use to change the world if you are planning for a lifetime, educate people............", new Date(2021, 1, 20)),
+    new Quote(1, 'Gideon Maroko', "“Education is the most powerful weapon which you can use to change the world if you are planning for a lifetime, educate people............", new Date(2021, 1, 20)),
     new Quote(2, 'Stephine Ndele', "“If You are planning for a year, sow rice; if you are planning for a decade, plant trees; if you are planning for a lifetime, educate people”", new Date(2021, 1, 12)),
     new Quote(3, 'David Hashisoma', "“When you educate one person you can change a life, when you educate many you can change the world if you are planning for a lifetime, educate”", new Date(2021, 1, 17)),
-    new Quote(4, 'Breen Mkaka', "“The first step toward success is taken when you refuse to be a captive of the environment in which you first find yourself.” ", new Date(2021, 2, 15)),
+    new Quote(4, 'Rose Okoth ', "“The first step toward success is taken when you refuse to be a captive of the environment in which you first find yourself.” ", new Date(2021, 2, 15)),
   ];
 
   like(index) {
